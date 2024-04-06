79b3674e2e003bdeaea080d3e387b9838cd2f22a
Ulad <vladis67948@gmail.com>
2024-04-06 20:10:00 +0300

    feat(app): added connect mongo base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { getMongoDBConfig } from './config/mongo.config'
+import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoDBConfig
+		}),
+		AuthModule,
+		UserModule
+	],
+	controllers: [AppController, UserController],
+	providers: [AppService, UserService]
 })
 export class AppModule {}
